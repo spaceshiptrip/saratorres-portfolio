3d993f3c75a26ae044eefc1a26738dc3116b1c58
spaceshiptrip <4379326+spaceshiptrip@users.noreply.github.com>
2024-09-15 21:35:14 -0700

    still trying to fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Papers from './components/Papers';
@@ -32,4 +32,3 @@ function App() {
 }
 
 export default App;
-
